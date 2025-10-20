e560191dc1afe13852fd25d24825c20916554b2f
AUTeddy <edis.zunic@gmx.net>
2025-10-20 11:46:26 +0200

    send_test_message
    Switch to EventBridge

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,21 @@
 extends Control
 
+func _exit_tree() -> void:
+	# Disconnect all Events
+	EventManager.off_MainMenu()
+
+func _ready() -> void:
+	
+	# You can use it with a lambda func, just add you args as well
+	#EventManager.on_mainmenu_send_test_message(func(message):
+		#print("Message [%s] received on peer [%s], from peer [%s]." % 
+		#[message, 
+		#get_tree().get_multiplayer().get_unique_id(), 
+		#get_tree().get_multiplayer().get_remote_sender_id()])
+	#)
+	
+	# And you can define a func
+	EventManager.on_mainmenu_send_test_message(_send_test_message)
 
 func _on_host_game_pressed() -> void:
 	NetworkManager.create_server()
@@ -10,9 +26,10 @@ func _on_join_game_pressed() -> void:
 	NetworkManager.create_client()
 
 func _on_send_test_message_pressed() -> void:
-	_send_test_message.rpc("Hello there!")
+	#_send_test_message.rpc("Hello there!")
+	EventManager.mainmenu_send_test_message("Hello there", get_tree().get_multiplayer().get_remote_sender_id())
 
-@rpc("any_peer", "call_remote")
+#@rpc("any_peer", "call_remote")
 func _send_test_message(message: String):
 	print("Message [%s] received on peer [%s], from peer [%s]." % 
 		[message, 
